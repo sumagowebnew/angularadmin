6eef24aff00ce294f882686ab4aec6150b4deab4
aadu238 <aditisonawane2308@gmail.com>
2023-04-21 14:04:02 +0530

    modules deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SideNavComponent } from './side-nav/side-nav.component';
-import { MainComponent } from './main/main.component';
-import { TopWidgetsComponent } from './top-widgets/top-widgets.component';
-import { SalesByMonthComponent } from './sales-by-month/sales-by-month.component';
-import { SalesByCategoryComponent } from './sales-by-category/sales-by-category.component';
-import { LastFewTransactionsComponent } from './last-few-transactions/last-few-transactions.component';
-import { TopThreeProductsComponent } from './top-three-products/top-three-products.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LoginComponent } from './auth-components/login/login.component';
 import { NotFoundComponent } from './auth-components/not-found/not-found.component';
@@ -44,14 +36,8 @@ import { EventAddComponent } from './modules/main/event-add/event-add.component'
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    SideNavComponent,
-    MainComponent,
-    TopWidgetsComponent,
-    SalesByMonthComponent,
-    SalesByCategoryComponent,
-    LastFewTransactionsComponent,
-    TopThreeProductsComponent,
+    
+   
     //auth modules
     LoginComponent,
     NotFoundComponent,
