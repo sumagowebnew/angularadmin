78bc331458c62aa375abb21d51fb25b3b6224282
aadu238 <aditisonawane2308@gmail.com>
2023-05-24 14:01:34 +0530

    component design updates

--- src/app/modules/main/team/team.component.ts
+++ src/app/modules/main/team/team.component.ts
@@ -7,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./team.component.scss']
 })
 export class TeamComponent {
+  admins:any[]=[
+    {
+      name:'Demo Name',
+      img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLYtLDlvHW2qWZR_qy8PmZ_rz-TPcVUGidsLRfe_Ck&s',
+      designation:'Team Leader',
+      qulification:'MSC. Computer Science',
+      experience:'Industry:6 year',
+      status:'Active',
+      date: '2023-04-12'
+    },
+    {
+      name:'Aemo Name',
+      img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLYtLDlvHW2qWZR_qy8PmZ_rz-TPcVUGidsLRfe_Ck&s',
+      designation:'Learning and Development',
+      qulification:'BSC Computer Science',
+      experience:'Industry:5 year',
+      status:'Active',
+      date: '2023-05-12'
+    }
 
+  ];
+  constructor() { }
+
+  getAdmin() {
+    return this.admins.slice();
+  }
+
+  addAdmin(index: number) {
+    this.admins.push(index);
+  }
+
+  updateAdmin(index: number,) {
+    this.admins[index] = 1;
+  }
+  editcontact(index: number): void {
+  }
+  deletecontact(index: number){
+    this.admins.splice(index, 1);
+  }
 }
