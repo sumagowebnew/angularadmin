92a2a39c5dee5fd4637c863b9855bf37593e19c3
purvadiwate <purvadiwate1459@gmail.com>
2023-06-09 10:56:47 +0530

    changes in component

--- src/app/modules/main/add-appreciation/add-appreciation.component.ts
+++ src/app/modules/main/add-appreciation/add-appreciation.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ImageService } from 'src/app/services/image.service';
 
 @Component({
   selector: 'app-add-appreciation',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-appreciation.component.scss']
 })
 export class AddAppreciationComponent {
+  fileForm = new FormGroup({
+    fileInput: new FormControl('',Validators.required)
+  });
+
+  @ViewChild('fileInput') fileInput: ElementRef<HTMLInputElement> | undefined;
+
+  constructor(private http: HttpClient,private imageService:ImageService) { }
+
+  onUpload(): void {
+    const fileInput = this.fileInput?.nativeElement;
+    if (fileInput && fileInput.files && fileInput.files[0]) {
+      const file = fileInput.files[0];
+      this.imageService.convertImageToBase64(file, (base64String, error) => {
+        if (error) {
+          console.error(error);
+        } else {
+          this.imageService.saveImageToServer(base64String);
+        }
+      });
+    }
+  }
 
 }
